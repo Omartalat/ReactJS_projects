cde1a32443d429ea38a303ff7bf5f2f6d0083402
omar-talat <dr.omartalat@gmail.com>
2025-11-11 21:58:25 +0200

    feat: enhance UserInput component with state management and input handling

--- investment_calculator/src/App.jsx
+++ investment_calculator/src/App.jsx
@@ -1,5 +1,6 @@
 import Header from "./components/Header";
 import UserInput from "./components/UserInput";
+
 function App() {
   return (
     <>
--- investment_calculator/src/components/UserInput.jsx
+++ investment_calculator/src/components/UserInput.jsx
@@ -1,19 +1,70 @@
+import { useState } from "react";
+import { calculateInvestmentResults } from "../util/investment";
+
 export default function UserInput() {
+  const [investmenData, setInvestmentData] = useState({
+    initialInvestment: "",
+    annualInvestment: "",
+    expectedReturn: "",
+    duration: "",
+  });
+
+  function handleChange(e) {
+    const { name, value } = e.target;
+
+    setInvestmentData((prevInvestmentData) => ({
+      ...prevInvestmentData,
+      [name]: value,
+    }));
+  }
+  const annualData = calculateInvestmentResults(investmenData);
+
   return (
-    <div className="input-group">
-      <div id="user-input">
-        <label htmlFor="initial_investment">Initial Investment</label>
-        <input id="initial_investment" type="number" name="initial_investment" />
+    <>
+      <div className="input-group">
+        <div id="user-input">
+          <label htmlFor="initial_investment">Initial Investment</label>
+          <input
+            id="initial_investment"
+            type="number"
+            name="initialInvestment"
+            onChange={handleChange}
+          />
 
-        <label htmlFor="annual_investment">Annual Investment</label>
-        <input id="annual_investment" type="number" name="annual_investment" />
-      
-        <label htmlFor="expected_return">Expected Return</label>
-        <input id="expected_return" type="number" name="expected_return" />
+          <label htmlFor="annual_investment">Annual Investment</label>
+          <input
+            id="annual_investment"
+            type="number"
+            name="annualInvestment"
+            onChange={handleChange}
+          />
 
-        <label htmlFor="duration">Duration</label>
-        <input id="duration" type="number" name="duration" />
+          <label htmlFor="expected_return">Expected Return</label>
+          <input
+            id="expected_return"
+            type="number"
+            name="expectedReturn"
+            onChange={handleChange}
+          />
+
+          <label htmlFor="duration">Duration</label>
+          <input
+            id="duration"
+            type="number"
+            name="duration"
+            onChange={handleChange}
+          />
+        </div>
       </div>
-    </div>
+      <table id="result">
+        <tr>
+          <th>Year</th>
+          <th>Ivestment Value</th>
+          <th>Interest (Year)</th>
+          <th>Total Interest</th>
+          <th>Invested Capital</th>
+        </tr>
+      </table>
+    </>
   );
 }
