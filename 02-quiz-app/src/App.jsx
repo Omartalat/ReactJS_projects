0ed4bed2b09ccc7762ff2125b3528d9a49ca9317
omar-talat <dr.omartalat@gmail.com>
2025-12-09 20:00:49 +0200

    feat: implement basic layout for quiz app with header and logo

--- 02-quiz-app/src/App.jsx
+++ 02-quiz-app/src/App.jsx
@@ -1,3 +1,11 @@
-function App() {}
-
+function App() {
+  return (
+    <>
+      <header>
+        <img src="quiz-logo.png" alt="quiz logo" />
+        <h1>ReactQuiz</h1>
+      </header>
+    </>
+  );
+}
 export default App;
